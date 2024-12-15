e93bac2283b01a79d684c36772de29fb239693c3
Muhammad Ilyas Zaini <qiyas.iq@gmail.com>
2024-12-16 03:52:20 +0700

    tambah graph di laporan

--- admin/report.php
+++ admin/report.php
@@ -1,3 +1,5 @@
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 <?php session_start();
 if(empty($_SESSION['nama'])){ ?>
     <script> window.location.href='../index.php' </script>
@@ -6,6 +8,42 @@ $nama = $_SESSION['nama'];
 if($_SESSION['hak'] == 'admin'){}else{ ?> <script> alert('Anda Bukan Admin!'); window.location.href='../logout.php' </script> <?php } 
 include "../conf/connection.php";
 $show='none';
+
+function bindingarr($arr){
+    $result=[];
+    foreach($arr as $data){
+        $result[$data['waktu_transaksi']]=$data['subtotal'];
+    }
+    return $result;
+}
+
+function totalByDay($array){
+    $result=[];
+    foreach($array as $datkey => $data){
+        if(! array_key_exists($datkey,$result)){
+            $result[$datkey]=$data;
+        }else{
+            $result[$datkey]+=$data;
+        }
+    }
+    return $result;
+
+}
+
+function ambiltangal($array){
+    $result=[];
+    foreach($array as $key=> $data){
+        $result[]=$key;
+    }
+    return $result;
+}
+function ambiltotal($array){
+    $result=[];
+    foreach($array as $key => $data){
+        $result[]=$data;
+    }
+    return $result;
+}
  ?>
 <!DOCTYPE html>
 <html>
@@ -61,11 +99,11 @@ $show='none';
           <div class="row">
               <div class="col-lg-3">
               <label for="tglawal" class="form-lable">tanggal awal</label>
-              <input type="date" class="form-control" placeholder="tanggal awal" name="tanggalawal">
+              <input type="date" class="form-control" placeholder="tanggal awal" name="tanggalawal" value="<?php if(isset($_POST['tanggalawal'])){ echo $_POST['tanggalawal'];}?>">
           </div>
           <div class="col-lg-3">
             <label for="tglakhir" class="form-lable">tanggal akhir</label>
-              <input type="date" class="form-control" id="tglakhir" placeholder="tanggal akhir" name="tanggalakhir">
+              <input type="date" class="form-control" id="tglakhir" placeholder="tanggal akhir" name="tanggalakhir" value="<?php if(isset($_POST['tanggalakhir'])){ echo $_POST['tanggalakhir'];}?>">
           </div>
                   <button type="submit" class="btn btn-primary mt-4 m-2">lihat laporan Transaksi</button>
               </div>
@@ -73,31 +111,67 @@ $show='none';
           </form>
        </div>
     </div>
-<?php 
+<tal?php 
 if($_SERVER['REQUEST_METHOD']=='POST'){
     $tglawal=$_POST['tanggalawal'];
     $tglakhir=$_POST['tanggalakhir'];
-    $query="SELECT * FROM transaksi WHERE (status_transaksi= 'lunas') AND (waktu_transaksi BETWEEN '$tglawal' AND '$tglakhir')";
+    $query="SELECT waktu_transaksi,subtotal FROM transaksi WHERE (status_transaksi= 'lunas') AND (waktu_transaksi BETWEEN '$tglawal' AND '$tglakhir')";
     $result=mysqli_query($connect,$query);
     $data=mysqli_fetch_all($result,MYSQLI_ASSOC);
     //var_dump($data);
+    $tabeldata=totalByDay( bindingarr($result));
     $show='block';
 }
 ?>
     <div class="container">
       
 <div style="display: <?= $show?>">
+    
+    <div class="d-flex justify-content-center">
+    
+    <div class="w-65 p-3 ">
+        
+        <div>
+        <canvas id="myChart"></canvas>
+        </div>
+    
+        <script>
+        const ctx = document.getElementById('myChart');
+    
+        new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: <?= json_encode( ambiltangal($tabeldata))?>,
+            datasets: [{
+            label: '# of Votes',
+            data: <?= json_encode( ambiltotal($tabeldata))?>,
+            borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        }
+        });
+    </script>
+    
+    </div>
+    </div>
+
        <table id="tables" class="table table-responsive table-bordered table-striped">
            <thead>
                <tr>
                    <th style="text-align: center;"> Tanggal  </th>
-                   <th style="text-align: center;"> Subtotal </th>
+                   <th style="text-align: center;"> total perhari </th>
                </tr>
            </thead>
-           <?php foreach($data as $dat):?>
+           <?php foreach($tabeldata as $key =>$dat):?>
                    <tr>
-                       <td style="text-align: center;"><?= $dat['waktu_transaksi'] ?></td>
-                       <td style="text-align: center;"><?= $dat['subtotal'] ?></td>
+                       <td style="text-align: center;"><?= $key ?></td>
+                       <td style="text-align: center;"><?= $dat ?></td>
                    </tr>
             <?php endforeach;?>
        </table>
